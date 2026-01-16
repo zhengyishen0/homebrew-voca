b334dd8401bfc922b01aaf17e86339631dd17afa
Zhengyi Shen <zhengyishen1@gmail.com>
2026-01-16 17:39:36 +0800

    Initial Homebrew tap setup

---
+++ Casks/voca.rb
@@ -0,0 +1,18 @@
+cask "voca" do
+  version "0.0.0"
+  sha256 "placeholder"
+
+  url "https://github.com/zhengyishen0/voca-app/releases/download/v#{version}/Voca-#{version}.dmg"
+  name "Voca"
+  desc "Voice-to-text transcription for macOS menu bar"
+  homepage "https://github.com/zhengyishen0/voca-app"
+
+  depends_on macos: ">= :ventura"
+
+  app "Voca.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.zhengyishen.voca.plist",
+    "~/Library/Application Support/Voca",
+  ]
+end
